d99d0deb3ae90da02ce8336a837402562750290e
Andres D' Amelio <andresdamelio24@gmail.com>
2020-09-16 23:05:42 -0400

    secure asset

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
 
         <title>Laravel</title>
 
-        <link href="{{asset('css/app.css')}}" rel="stylesheet">
+        <link href="{{secure_asset('css/app.css')}}" rel="stylesheet">
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -26,6 +26,6 @@
         <div id="app" class="content">
 
         </div>
-        <script src="{{asset('js/app.js')}}"></script>
+        <script src="{{secure_asset('js/app.js')}}"></script>
     </body>
 </html>
